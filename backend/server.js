4aa39fcc44557082a4ff59967dcb9a170cbd6d6e
ayush <ayushgoswami.abr@gmail.com>
2022-06-23 01:02:01 +0530

    Final changes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const connectdb = require("./config/db");
@@ -12,6 +13,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalsroute"));
 app.use("/api/users", require("./routes/userroute"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html");
+  });
+} else {
+  app.get("/", (req, res) => res.send("please set to production"));
+}
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
